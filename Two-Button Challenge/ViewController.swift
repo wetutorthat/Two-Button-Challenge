2aacbec0f835ba0d3420ebdbc45385ae3eddb22d
Larry Goodman <wetutorthat@gmail.com>
2021-06-25 12:48:13 -0400

    Added Message Label and 2 buttons to change text in Message Label.

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+    }
+    
+    @IBAction func showAnotherMessagedPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+    }
+    
 }
 
